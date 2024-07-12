aa48894702095d211540fd944ad089197e9dbfda
sovisrushain <sovisrushain@gmail.com>
2024-07-12 22:49:54 +0530

    initial commit

---
+++ src/main/java/com/cisco/bpm/BpmApplication.java
@@ -0,0 +1,13 @@
+package com.cisco.bpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpmApplication.class, args);
+	}
+
+}
